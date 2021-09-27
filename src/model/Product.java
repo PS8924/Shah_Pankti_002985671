6b97b8b8ba3011f3949f791c9bbcf906ddbee3af
shah pankti <shah.pankti@northeastern.edu>
2021-09-26 23:30:40 -0400

    Submitting AED Assignment 1

---
+++ src/model/Product.java
@@ -0,0 +1,197 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author Shah's
+ */
+public class Product {
+ 
+    private String name;
+    private String geoData;
+    private String dateOfBirth;
+    private long telephoneNum;
+    private long faxNum;
+    private String email;
+    private long ssn;
+    private long medRecNum;
+    private long healthPlanNum;
+    private long bankAccNum;
+    private String licenceNum;
+    private String vehicleLicPlate;
+    private String deviceIdenNum;
+    private String linkedIn;
+    private String ipAdd;
+    private String biometricIdentifier;
+    private String photo;
+    private String uniqueID;
+    private String bioImagePath;
+    private String photoImagePath;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGeoData() {
+        return geoData;
+    }
+
+    public void setGeoData(String geoData) {
+        this.geoData = geoData;
+    }
+
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(String dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public long getTelephoneNum() {
+        return telephoneNum;
+    }
+
+    public void setTelephoneNum(long telephoneNum) {
+        this.telephoneNum = telephoneNum;
+    }
+
+    public long getFaxNum() {
+        return faxNum;
+    }
+
+    public void setFaxNum(long faxNum) {
+        this.faxNum = faxNum;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public long getSsn() {
+        return ssn;
+    }
+
+    public void setSsn(long ssn) {
+        this.ssn = ssn;
+    }
+
+    public long getMedRecNum() {
+        return medRecNum;
+    }
+
+    public void setMedRecNum(long medRecNum) {
+        this.medRecNum = medRecNum;
+    }
+
+    public long getHealthPlanNum() {
+        return healthPlanNum;
+    }
+
+    public void setHealthPlanNum(long healthPlanNum) {
+        this.healthPlanNum = healthPlanNum;
+    }
+
+    public long getBankAccNum() {
+        return bankAccNum;
+    }
+
+    public void setBankAccNum(long bankAccNum) {
+        this.bankAccNum = bankAccNum;
+    }
+
+    public String getLicenceNum() {
+        return licenceNum;
+    }
+
+    public void setLicenceNum(String licenceNum) {
+        this.licenceNum = licenceNum;
+    }
+
+    public String getVehicleLicPlate() {
+        return vehicleLicPlate;
+    }
+
+    public void setVehicleLicPlate(String vehicleLicPlate) {
+        this.vehicleLicPlate = vehicleLicPlate;
+    }
+
+    public String getDeviceIdenNum() {
+        return deviceIdenNum;
+    }
+
+    public void setDeviceIdenNum(String deviceIdenNum) {
+        this.deviceIdenNum = deviceIdenNum;
+    }
+
+    public String getLinkedIn() {
+        return linkedIn;
+    }
+
+    public void setLinkedIn(String linkedIn) {
+        this.linkedIn = linkedIn;
+    }
+
+    public String getIpAdd() {
+        return ipAdd;
+    }
+
+    public void setIpAdd(String ipAdd) {
+        this.ipAdd = ipAdd;
+    }
+
+    public String getBiometricIdentifier() {
+        return biometricIdentifier;
+    }
+
+    public void setBiometricIdentifier(String biometricIdentifier) {
+        this.biometricIdentifier = biometricIdentifier;
+    }
+    
+    public String getbioImagePath() {
+        return bioImagePath;
+    }
+
+    public void setBioImagePath(String bioImagePath) {
+        this.bioImagePath = bioImagePath;
+    }
+    
+    public String getphotoImagePath() {
+        return photoImagePath;
+    }
+
+    public void setPhotoImagePath(String photoImagePath) {
+        this.photoImagePath = photoImagePath;
+    }
+
+    public String getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(String photo) {
+        this.photo = photo;
+    }
+
+    public String getUniqueID() {
+        return uniqueID;
+    }
+
+    public void setUniqueID(String uniqueID) {
+        this.uniqueID = uniqueID;
+    }
+    
+    
+
+}
